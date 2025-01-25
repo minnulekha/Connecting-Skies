b46d74284abf7181fe455745d6670cd5d247d65b
minnulekha <minnulekhavg@gmail.com>
2025-01-26 03:39:45 +0530

    designed movie page

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port=3002;
 app.use(express.static('public'))
 app.get('/',(req,res)=>{res.sendFile(path.join(__dirname,'public/index.html'));})
 
-app.get('/entertainment',(req,res)=>{res.sendFile(path.join(__dirname,'public/entertainment.html'));})
+app.get('/movies',(req,res)=>{res.sendFile(path.join(__dirname,'public/movies.html'));})
 
 app.get('/advice',(req,res)=>{res.sendFile(path.join(__dirname,'public/advice.html'));})
 app.get('/about',(req,res)=>{res.sendFile(path.join(__dirname,'public/about.html'));})
