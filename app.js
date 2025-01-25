5a68794a12a8761e490d5d6263226a11f9123104
minnulekha <minnulekhavg@gmail.com>
2025-01-25 17:49:54 +0530

    Page Created

---
+++ app.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const app=express();
+const path=require('path');
+const port=3002;
+app.use(express.static('public'))
+app.get('/',(req,res)=>{res.sendFile(path.join(__dirname,'public/index.html'));})
+
+app.get('/entertainment',(req,res)=>{res.sendFile(path.join(__dirname,'entertainment.html'));})
+
+app.get('/advice',(req,res)=>{res.sendFile(path.join(__dirname,'public/advice.html'));})
+app.get('/about',(req,res)=>{res.sendFile(path.join(__dirname,'public/about.html'));})
+app.get('/login',(req,res)=>{res.sendFile(path.join(__dirname,'public/login.html'));})
+
+
+
+
+app.listen(port,()=>{console.log(`app listening on port:${port}`);})
